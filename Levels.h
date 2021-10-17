7e39ec28bc706c0456ff646cd1bae8cc9968608c
Megarev <64357296+Megarev@users.noreply.github.com>
2021-10-17 18:40:45 +0530

    Added a demo level

---
+++ Levels.h
@@ -0,0 +1,123 @@
+#pragma once
+#include "polygon.h"
+#include "constraints.h"
+
+class Level {
+private:
+	Level() {}
+public:
+	static Level Get() {
+		static Level level;
+		return level;
+	}
+
+	void DemoScene(std::vector<PolygonShape>& polygons, ConstraintManager& constraint_mgr, JointPairManager& joint_mgr, uint32_t& id_count) {
+
+		auto AddPolygon = [&](int n_vertices, const olc::vf2d& pos, const olc::Pixel& color, const std::vector<olc::vf2d>& vertices,
+			float mass, float e, float sf, float df, float w = 0.0f) -> void {
+				polygons.push_back(PolygonShape{ n_vertices, olc::vf2d{ 1.0f, 1.0f }, pos, color, id_count++ });
+				for (int i = 0; i < polygons.back().n_vertices; i++) {
+					polygons.back().GetVertex(i) = vertices[i];
+				}
+				polygons.back().properties.mass = mass;
+				polygons.back().properties.e = e;
+				polygons.back().properties.sf = sf;
+				polygons.back().properties.df = df;
+				polygons.back().properties.angular_velocity = w;
+		};
+
+		AddPolygon(4, { 162.0f, 420.0f }, olc::DARK_CYAN, {
+			{ 471.301, 442.95 },
+			{ 139.051, 442.301 },
+			{ 139.699, 397.05 },
+			{ 472.95, 398.699 }
+			}, 0.0f, 1.0f, 0.5f, 0.2f, 0.0f
+		);
+
+		AddPolygon(4, { 199.0f, 375.0f }, olc::CYAN, {
+			{ 221.627, 397.627 },
+			{ 176.373, 397.627 },
+			{ 176.373, 352.373 },
+			{ 221.627, 352.373 },
+			}, 10.0f, 1.0f, 0.5f, 0.2f, 0.0f
+			);
+
+		AddPolygon(4, { 408.0f, 376.0f }, olc::CYAN, {
+			{ 430.627, 398.627 },
+			{ 385.373, 398.627 },
+			{ 385.373, 353.373 },
+			{ 430.627, 353.373 },
+			}, 10.0f, 1.0f, 0.5f, 0.2f, 0.0f
+			);
+
+		AddPolygon(4, { 200.0f, 332.0f }, olc::CYAN * 0.8f, {
+			{ 430.731, 351.523 },
+			{ 177.477, 354.731 },
+			{ 177.269, 309.477 },
+			{ 430.523, 309.269 },
+			}, 8.0f, 1.0f, 0.5f, 0.2f, 0.0f
+			);
+
+		AddPolygon(4, { 246.0f, 287.0f }, olc::CYAN * 0.6f, {
+			{ 268.627, 309.627 },
+			{ 223.373, 309.627 },
+			{ 223.373, 264.373 },
+			{ 268.627, 264.373 },
+			}, 5.0f, 1.0f, 0.5f, 0.2f, 0.0f
+			);
+
+		AddPolygon(4, { 360.0f, 285.0f }, olc::CYAN * 0.6f, {
+			{ 382.627, 307.627 },
+			{ 337.373, 307.627 },
+			{ 337.373, 262.373 },
+			{ 382.627, 262.373 },
+			}, 5.0f, 1.0f, 0.5f, 0.2f, 0.0f
+			);
+
+		AddPolygon(4, { 312.0f, 237.0f }, olc::CYAN * 0.5f, {
+			{ 387.567, 263.996 },
+			{ 221.307, 261.386 },
+			{ 260.422, 218.235 },
+			{ 355.687, 220.619 },
+			}, 2.0f, 1.0f, 0.5f, 0.2f, 0.0f
+			);
+
+		AddPolygon(3, { 306.0f, 202.0f }, olc::YELLOW, {
+			{ 332.924, 219.295 },
+			{ 277.56, 216.669 },
+			{ 307.516, 170.036 },
+			}, 1.0f, 1.0f, 0.5f, 0.2f, 0.0f
+			);
+
+		AddPolygon(10, { 551.0f, 115.0f }, olc::MAGENTA, {
+			{ 573.627, 137.627 },
+			{ 556.006, 146.606 },
+			{ 536.472, 143.512 },
+			{ 522.488, 129.528 },
+			{ 519.394, 109.994 },
+			{ 528.373, 92.3726 },
+			{ 545.994, 83.394 },
+			{ 565.528, 86.4878 },
+			{ 579.512, 100.472 },
+			{ 582.606, 120.006 }
+			}, 50.0f, 1.0f, 0.5f, 0.2f, 0.0f
+		);
+
+		AddPolygon(10, { 417.0f, 56.0f }, olc::BLUE, {
+			{ 439.627, 78.6274 },
+			{ 422.006, 87.606 },
+			{ 402.472, 84.5122 },
+			{ 388.488, 70.5277 },
+			{ 385.394, 50.9941 },
+			{ 394.373, 33.3726 },
+			{ 411.994, 24.394 },
+			{ 431.528, 27.4878 },
+			{ 445.512, 41.4723 },
+			{ 448.606, 61.0059 },
+			}, 1.0f, 1.0f, 0.5f, 0.2f, 1.0f
+			);
+
+		constraint_mgr.constraints_data.push_back({ 8, { { 551.0f, 115.0f }, { 287.0f, 62.0f } }, 0.5f, 0.2f });
+		joint_mgr.data.push_back({ { 8, 9 }, { { 551.0f,115.0f }, { 417.0f,56.0f } }, 0.5f, 0.2f });
+	}
+};
