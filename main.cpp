ff3bff2727b54153252223caa4ac24183acd4e01
Megarev <64357296+Megarev@users.noreply.github.com>
2021-10-19 23:18:50 +0530

    Added a splash screen

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include "olcPixelGameEngine.h"
 #define PHYSICS_H
 #include "editor.h"
+#define OLC_PGEX_SPLASHSCREEN
+#include "olcPGEX_SplashScreen.h"
 
 class App : public olc::PixelGameEngine {
 private:
 	Editor editor;
+	olcPGEX_SplashScreen splash_screen;
 public:
 	App() {
-		sAppName = "Title";
+		sAppName = "Polygon Sandbox";
 	}
 
 	bool OnUserCreate() override {
@@ -16,10 +19,13 @@ public:
 		editor = Editor{ this };
 		editor.DrawBackground();
 
+		splash_screen.SetOptions(4, 2, 2.0f, 0.5f, olc::CYAN * 0.1f, olc::CYAN, olc::YELLOW, olc::WHITE);
+
 		return true;
 	}
-
 	bool OnUserUpdate(float dt) override {
+
+		if (splash_screen.AnimateSplashScreen(dt)) return true;
 
 		// Window Update
 		editor.WindowUpdate();
